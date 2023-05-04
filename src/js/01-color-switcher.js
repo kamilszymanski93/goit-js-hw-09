f55df79e4d293561d4ddbad760973f7ed094dc57
Kamil Szymański <kamil313313@gmail.com>
2023-05-04 22:06:17 +0200

    HW9 T1 Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,21 @@
-const startButton = document.querySelector('data-start');
-const stopButton = document.querySelector('data-stop');
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let intervalFn = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  intervalFn = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  stopButton.disabled = true;
+  startButton.disabled = false;
+  clearInterval(intervalFn);
+});
