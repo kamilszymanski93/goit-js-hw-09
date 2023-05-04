2e98e679bc294059b280c55a528b5fec327a36c8
kamilszymanski93 <109624557+kamilszymanski93@users.noreply.github.com>
2023-05-04 19:31:21 +0000

    Deploying to gh-pages from @ kamilszymanski93/goit-js-hw-09@bfe291b87eb157d8209437dc9f729c5ac177a322 🚀

---
+++ 01-color-switcher.c222206b.js
@@ -0,0 +1,2 @@
+document.querySelector("data-start"),document.querySelector("data-stop");
+//# sourceMappingURL=01-color-switcher.c222206b.js.map
---
+++ 01-color-switcher.df9d7e63.js
@@ -0,0 +1,2 @@
+document.querySelector("data-start"),document.querySelector("data-stop");
+//# sourceMappingURL=01-color-switcher.df9d7e63.js.map
